63520dd402cb8d75ef8df74db56d5ac817cb9777
Denis Matvienko <grabsomebuds27@gmail.com>
2023-02-11 19:39:15 +0300

    add stylelint, fix styles, add i18n plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
+/*
+  line-breaks:
+  linebreak-style': ['error', 'windows'] - (in vscode use crlf in this file)
+  (linebreak in windows looks is - \r\n)
+  https://eslint.org/docs/latest/rules/linebreak-style
+  https://stackoverflow.com/questions/39114446/how-can-i-write-a-eslint-rule-for-linebreak-style-changing-depending-on-windo
+
+  react/jsx-indent - 4 - indents
+  'no-unused-vars': 'warn' - use warning when var is unused
+  'react/react-in-jsx-scope': 'off' - after React 17, should off
+  'react/jsx-props-no-spreading': 'warn' - spread for props is bad, except ui components
+  'react/function-component-definition': 'off' -
+  means that should use function declaration instead of arrow func in component
+
+*/
+
 module.exports = {
   env: {
     browser: true,
@@ -6,6 +22,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:i18next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -23,26 +40,26 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'i18next',
   ],
   rules: {
-    'react/jsx-indent': [2, 4], // 4 - indents
+    'react/jsx-indent': [2, 4],
     'react/jsx-indent-props': [2, 4],
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
-    // https://eslint.org/docs/latest/rules/linebreak-style
-    // https://stackoverflow.com/questions/39114446/how-can-i-write-a-eslint-rule-for-linebreak-style-changing-depending-on-windo
-    'linebreak-style': ['error', 'windows'], // use crlf in this file (linebreack in windows is - \r\n)
+    'linebreak-style': ['error', 'windows'],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    'no-unused-vars': 'warn', // use warning when var is unused
+    'no-unused-vars': 'warn',
     'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off', // after React 17, should off
-    'react/jsx-props-no-spreading': 'warn', // spread for props is bad, except ui components
-    'react/function-component-definition': 'off', // means that should use function declaration instead of arrow func in component
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
     'no-shadow': 'off',
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'react/jsx-props-no-multi-spaces': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -5,13 +5,13 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-// .use(Backend)
-// .use(LanguageDetector)
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'ru',
     // __IS_DEV__ is - DefinePlugin var
-    debug: !!__IS_DEV__,
+    debug: __IS_DEV__,
 
     interpolation: {
       escapeValue: false,
