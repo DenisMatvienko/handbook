28f2e3907f23b77aa6e80f314f453fa18a7037b6
Denis Matvienko <grabsomebuds27@gmail.com>
2023-02-21 21:44:05 +0300

    fix dev plugins

--- configs/build/buildPlugins.ts
+++ configs/build/buildPlugins.ts
@@ -5,15 +5,14 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ isDev, paths }:
-  BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    /* Plugins list */
+export function buildPlugins({ isDev, paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
+    // Plugins list
     new HTMLWebpackPlugin({
-      /* Add root template */
+      // Add root template
       template: paths.html,
     }),
-    /* Add index.html into build folder */
+    // Add index.html into build folder
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
@@ -21,14 +20,24 @@ export function buildPlugins({ isDev, paths }:
     }),
     new webpack.DefinePlugin({
       // with help of DefinePlugin, we can add into app (like i18n) global variable
-      // firstble we need in i18n isDev check
+      // first we need in i18n isDev check
       __IS_DEV__: JSON.stringify(isDev),
     }),
-
-    new ReactRefreshWebpackPlugin(),
-    // https://www.npmjs.com/package/webpack-bundle-analyzer
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    // if is Dev mode we are add these plugins in build,
+    // but if prod-didn't.
+    // This check made for prevent addition BuildAnalyzerPlugin in to prod build
+    // because BuildAnalyzerPlugin running in GitHub actions, and still be running
+    // interfere successfully build bundle in actions
+    // https://www.npmjs.com/package/webpack-bundle-analyzer
+
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+
+  return plugins;
 }
