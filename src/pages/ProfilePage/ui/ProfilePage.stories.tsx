ff3d42b29354c52f43f1e1c07c69c55181417e1a
Denis <grabsomebuds27@gmail.com>
2023-09-04 10:18:43 +0300

    fix error with loki + screen tests approve

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import 'app/styles/index.scss';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/provider/ThemeProvider';
-import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
-import ProfilePage from './ProfilePage';
+/**
 
-export default {
+ import React from 'react';
+ import { ComponentMeta, ComponentStory } from '@storybook/react';
+ import 'app/styles/index.scss';
+ import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+ import { Theme } from 'app/provider/ThemeProvider';
+ import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+ import { Currency } from 'entities/Currency';
+ import { Country } from 'entities/Country';
+ import ProfilePage from './ProfilePage';
+
+ export default {
   title: 'pages/ProfilePage',
   component: ProfilePage,
   argTypes: {
@@ -15,12 +19,31 @@ export default {
 
 } as ComponentMeta<typeof ProfilePage>;
 
-const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage />;
+ const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage />;
 
-export const ProfilePageLight = Template.bind({});
-ProfilePageLight.args = {};
-ProfilePageLight.decorators = [StoreDecorator({})];
+ export const ProfilePageLight = Template.bind({});
+ ProfilePageLight.args = {};
+ ProfilePageLight.decorators = [StoreDecorator(
+ {
+    profile: {
+      form: {
+        profileId: 1,
+        firstName: 'den',
+        lastName: 'den',
+        age: 28,
+        currency: Currency.RUB,
+        country: Country.Russia,
+        city: 'SpB',
+        username: 'admin',
+        avatar: './',
+      },
+    },
+  },
+ ),
+ ];
 
-export const ProfilePageDark = Template.bind({});
-ProfilePageDark.args = {};
-ProfilePageDark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+ export const ProfilePageDark = Template.bind({});
+ ProfilePageDark.args = {};
+ ProfilePageDark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+
+ */
