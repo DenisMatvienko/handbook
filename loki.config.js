643c6bbaabd7f22622d95f089fd88509aeb1221e
Denis <grabsomebuds27@gmail.com>
2023-09-08 15:40:02 +0300

    loki config fix

--- loki.config.js
+++ loki.config.js
@@ -2,7 +2,7 @@
  *    Loki config
  *
  *    @param target
- *    - Always change 'chrome.app' on 'docker', when push code on GitHub
+ *    - Always change 'chrome.app' on 'chrome.docker', when push code on GitHub
  *      Reason in GitHub actions most likely their version of Loki does not support 'chrome.app'
  *      and test always fall with error.
  *    - When using local repo and use Loki change on 'chrome.app', cause docker on Windows
@@ -14,12 +14,12 @@ module.exports = {
   diffingEngine: 'pixelmatch',
   configurations: {
     'chrome.laptop': {
-      target: 'docker', // When pushing change on 'chrome.app' on 'docker'
+      target: 'chrome.docker', // When pushing on gh with actions change 'chrome.app' on 'chrome.docker'
       width: 1366,
       height: 768,
     },
     'chrome.iphone7': {
-      target: 'docker',
+      target: 'chrome.docker',
       preset: 'iPhone 7',
     },
   },
