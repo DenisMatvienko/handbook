0a1ed80af9e8847e01bf45db527aec19ad7e6cac
Denis Matvienko <grabsomebuds27@gmail.com>
2023-09-13 15:07:46 +0300

    add pink theme

--- src/app/provider/ThemeProvider/lib/ThemeContext.ts
+++ src/app/provider/ThemeProvider/lib/ThemeContext.ts
@@ -1,8 +1,18 @@
+/**
+ *      ThemeContext
+ *       -  Transmits themes to main component.
+ *
+ *       @param localStorage.
+ *        - Because we need save theme, even after user close the browser, and open again.
+ *          current theme should be saved.
+ */
+
 import { createContext } from 'react';
 
 export enum Theme {
     LIGHT = 'app_light_theme',
     DARK = 'app_dark_theme',
+    PINK = 'app_pink_theme',
 }
 
 export interface ThemeContextProps {
@@ -11,7 +21,4 @@ export interface ThemeContextProps {
 }
 
 export const ThemeContext = createContext<ThemeContextProps>({});
-
-// Local storage. Because we need save theme, even after user
-// close the browser, and open again. current theme should be saved
 export const LOCAL_STORAGE_THEME_KEY = 'theme';
