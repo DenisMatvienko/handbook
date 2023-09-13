bf25e7571dfc9a5a266b6fe39e620a6bf52943de
Denis Matvienko <grabsomebuds27@gmail.com>
2023-09-13 15:26:16 +0300

    test:unit, ui, lint check - pass

--- src/app/provider/ThemeProvider/lib/useTheme.ts
+++ src/app/provider/ThemeProvider/lib/useTheme.ts
@@ -31,7 +31,7 @@ export function useTheme(): UseThemeResult {
         newTheme = Theme.DARK;
         break;
       default:
-        newTheme = Theme.LIGHT
+        newTheme = Theme.LIGHT;
     }
 
     setTheme?.(newTheme);
