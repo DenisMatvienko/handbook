94c04a9877df8f18b7f23b04175ee8b58c731459
Denis Matvienko <grabsomebuds27@gmail.com>
2023-10-03 22:54:20 +0300

    article elements fixes

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -4,11 +4,7 @@
  */
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import React, { memo } from 'react';
-import {
-  Text, TextAlign, TextSize, TextTheme,
-} from 'shared/ui/Text/Text';
 import { ArticleCodeBlock } from 'entities/Article/model/types/article';
 import { Code } from 'shared/ui/Code/Code';
 import classes from './ArticleCodeBlockComponent.module.scss';
