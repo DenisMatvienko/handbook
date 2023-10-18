6ae0792b2bced8aeade1e269810942fac0d63898
Denis <grabsomebuds27@gmail.com>
2023-10-18 11:23:51 +0300

    trmporary delete layout of slice tests

--- src/pages/ArticleDetailsPage/model/slice/ArticleDetailsCommentsSlice.test.ts
+++ src/pages/ArticleDetailsPage/model/slice/ArticleDetailsCommentsSlice.test.ts
@@ -6,19 +6,19 @@
  *
  */
 
-import { add_slice_action, add_slice_reduser } from './ArticleDetailsCommentsSlice';
-import { add_type_schema } from '';
-
-const InitialState: DeepPartial<add_type_schema> = {};
-
-const data = {
-  also_can_add_manual_data,
-};
-
-describe('ArticleDetailsCommentsSlice', () => {
-  test('ArticleDetailsCommentsSlice reducer test', () => {
-  });
-
-  test('ArticleDetailsCommentsSlice reducer test', () => {
-  });
-});
+// import { add_slice_action, add_slice_reduser } from './ArticleDetailsCommentsSlice';
+// import { add_type_schema } from '';
+//
+// const InitialState: DeepPartial<add_type_schema> = {};
+//
+// const data = {
+//   also_can_add_manual_data,
+// };
+//
+// describe('ArticleDetailsCommentsSlice', () => {
+//   test('ArticleDetailsCommentsSlice reducer test', () => {
+//   });
+//
+//   test('ArticleDetailsCommentsSlice reducer test', () => {
+//   });
+// });
