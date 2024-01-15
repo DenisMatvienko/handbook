425b492554b80d45d18def81e752b86fd575b310
Denis <grabsomebuds27@gmail.com>
2024-01-15 11:22:21 +0300

    css loadenr ident name

--- configs/build/loaders/buildCssLoaders.ts
+++ configs/build/loaders/buildCssLoaders.ts
@@ -30,7 +30,7 @@ export function buildCssLoader(isDev : boolean) {
           modules: {
             auto: (resPath: string) => Boolean(resPath.includes('.module.')),
             localIdentName: isDev
-              ? '[path][name]__[local]--[hash:base64:5]'
+              ? '[path][name]--[local]--[hash:base64:5]'
               : '[local]--[hash:base64:8]',
           },
         },
