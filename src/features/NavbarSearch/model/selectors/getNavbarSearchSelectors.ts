3bb9a17278287af7e3c86e5570f6af9f2ee728c7
Denis <grabsomebuds27@gmail.com>
2024-02-07 10:28:24 +0300

    selector comm

--- src/features/NavbarSearch/model/selectors/getNavbarSearchSelectors.ts
+++ src/features/NavbarSearch/model/selectors/getNavbarSearchSelectors.ts
@@ -1,3 +1,8 @@
+/**
+ *     getNavbarSearchSelector
+ *      - get state of navbarSearch
+ */
+
 import { StateSchema } from 'app/provider/StoreProvider';
 
 export const getNavbarSearchSelector = (state: StateSchema) => state.navbarSearch?.articleList;
