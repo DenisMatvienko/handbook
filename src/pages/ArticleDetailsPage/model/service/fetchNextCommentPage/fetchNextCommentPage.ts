a2346ac8504f80ba189ad95b1cf41e6ecaf5e5b1
Denis Matvienko <grabsomebuds27@gmail.com>
2024-07-21 16:18:33 +0300

    update(comments): fetching fixes

--- src/pages/ArticleDetailsPage/model/service/fetchNextCommentPage/fetchNextCommentPage.ts
+++ src/pages/ArticleDetailsPage/model/service/fetchNextCommentPage/fetchNextCommentPage.ts
@@ -1,29 +1,18 @@
 /**
  *    fetchNextArticlePage fetching
- *      -
  */
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/provider/StoreProvider';
-import {
-  getArticlePageHasMore,
-  getArticlesPageIsLoading,
-  getArticlesPageNum,
-} from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
-import { articlePageSliceActions } from 'pages/ArticlesPage/model/slices/articlePageSlice';
-import {
-  fetchArticlesList,
-} from 'pages/ArticlesPage/model/services/fetchArticleList/fetchArticlesList';
+import { getUserAuthData } from 'entities/User';
 import {
   getArticleCommentsHasMore, getArticleCommentsIsLoading,
   getArticleCommentsPage,
 } from 'pages/ArticleDetailsPage/model/selectors/comments/GetComments';
-import { articleDetailsCommentsActions } from 'pages/ArticleDetailsPage/model/slice/ArticleDetailsCommentsSlice';
 import {
   fetchCommentsByArticleId,
 } from 'pages/ArticleDetailsPage/model/service/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import { useParams } from 'react-router-dom';
-import { getUserAuthData } from 'entities/User';
+import { articleDetailsCommentsActions } from 'pages/ArticleDetailsPage/model/slice/ArticleDetailsCommentsSlice';
 
 export const fetchNextCommentPage = createAsyncThunk<
     void,
